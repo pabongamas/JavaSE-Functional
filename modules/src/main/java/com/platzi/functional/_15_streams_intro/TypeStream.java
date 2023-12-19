89f7f94be633d04c9c1e02805b2ae9832ac44cd8
jhon <pabongamas@gmail.com>
2023-12-19 14:49:59 -0500

    actualizacion

--- modules/src/main/java/com/platzi/functional/_15_streams_intro/TypeStream.java
+++ modules/src/main/java/com/platzi/functional/_15_streams_intro/TypeStream.java
@@ -1,12 +1,15 @@
 package com.platzi.functional._15_streams_intro;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class TypeStream {
     public static void main(String[] args) {
         IntStream infiniteStream = IntStream.iterate(0, x -> x + 1);
-        infiniteStream.limit(1000)
+        List<Integer> numbersList=infiniteStream.limit(1000)
         /**con parallel las iteracciones se distribuyen por los nucleos que tenga la maquina 
          * recomendable para cuando sea procesamiento de cientos , millones de datos , 
          * si importa el orden de los datos lo mejor es no utilizar parallel
@@ -14,6 +17,10 @@ public class TypeStream {
         */
                 // .parallel()
                 .filter(x -> x % 2 == 0)
-                .forEach(System.out::println);
+                .boxed()
+                .collect(Collectors.toList());
+                // .forEach(System.out::println);
+
+                System.out.println(numbersList);
     }
 }
